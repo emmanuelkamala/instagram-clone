efe7fa761ac7fd3e8d03c386429374a7e54bd3b2
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-18 16:53:29 +0300

    Add aws configuration

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,18 @@ class PostsController < ApplicationController
 
   def create 
     @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to dashboard_path, flash: { success: "Post was successfully saved" }
+    else
+      redirect_to new_post_path, flash: { danger: "Post was not created" }
+    end
   end
 
   def Show
   end
 
   private 
-   
     def post_params
       params.require(:post).permit(:image, :image_cache)
     end
