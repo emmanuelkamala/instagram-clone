5bbdb77edcf9dd3eae67382178d3212dab305f98
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-19 12:52:51 +0300

    Connect account model to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_133926) do
+ActiveRecord::Schema.define(version: 2021_09_19_091143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 2021_09_17_133926) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username", limit: 20
+    t.string "first_name", limit: 20
+    t.string "last_name", limit: 20
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
