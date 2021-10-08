cbe910aaa797f4fa55ebf3f3f65295a7f61b5c42
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-10-08 13:59:15 +0300

    Add description table to posts and style it

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,12 @@ class Post < ApplicationRecord
   before_create :set_active
   scope :active, -> { where active: true }
 
+  def total_likes
+    0
+  end
+
+  private
+
   def set_active
     self.active = true
   end
