88da58c9e91fbfc2673c24f44bda5305866cbd85
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-10-08 04:31:38 +0300

    Add colums to accounts table

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :accounts
   get '/dashboard', to: "accounts#index"
+  get '/profile/:username', to: "accounts#profile", as: :profile
 
   resources :posts, only: [:new, :create, :show]
 end
